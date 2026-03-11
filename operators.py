4431779a8f3e4846b53d2ede9efd7826a22e2377
Prakhar <koshta215@gmail.com>
2026-03-11 15:24:59 +0530

    added operators and operators precedence

---
+++ operators.py
@@ -0,0 +1,27 @@
+a=10
+b=20
+c=30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#and also 
+a=a+10 #also written as 
+a+=10
+a-=10
+a*=10
+a/=10
+a==b
+a=10
+b=9
+#logical
+varia=1
+print(not(varia))# always return opposite 
+print((a>b)and(a>b))# both must be true 
+print((a<b)or(a>b))#any one of the given value can return true
