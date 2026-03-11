632a5bbc10f666f5bcddc18d90464a60dc04d5eb
Prakhar <koshta215@gmail.com>
2026-03-11 14:51:58 +0530

    ADDED DATA TYPE AND COMMENT CODE

---
+++ comments.py
@@ -0,0 +1,6 @@
+#for single line comments we use # sign 
+'''for multi line comments 
+we use triple apsostrophy sign in the start and end
+
+
+'''
--- data_types.py
+++ data_types.py
@@ -1,4 +1,7 @@
 name = "prak"
 age = 20
 add = "jbp"
+height=5.12
+prime=False
+print(type(name),type(age),type(height),type(prime))
 print("my name is:",name,"and age is:",age,"address is:", add)
