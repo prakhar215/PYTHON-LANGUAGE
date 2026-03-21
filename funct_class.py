989636af304b2e9da97e486658fbf3fbc6fdc7ec
Prakhar <koshta215@gmail.com>
2026-03-21 23:02:10 +0530

    function in class

---
+++ funct_class.py
@@ -0,0 +1,8 @@
+class person:
+    age = 20
+    name = "prak"
+    height = 6.0
+    def fun():
+        print(f"the person name is {"name"} and age is {"age"},height {"height"}")
+prakh=person()
+print(person.height)
