1dcbf65b7b4291e5e306ea8e49e46c89babac22f
Prakhar <koshta215@gmail.com>
2026-03-21 23:26:47 +0530

    constructor call

---
+++ constructor_call.py
@@ -0,0 +1,7 @@
+class mobile:
+    def __init__(self,model,price):
+        self.Model=model
+        self.Price=price
+        print("phone model is ",self.Model)
+nokia=mobile(1133,10000)
+apple=mobile(17,82000)
