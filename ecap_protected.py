c4f1bc55c883ba82c0a40bada659f889f8707d94
Prakhar <koshta215@gmail.com>
2026-03-24 20:43:12 +0530

    Added protected attribute

---
+++ ecap_protected.py
@@ -0,0 +1,8 @@
+class account:
+    def __init__(self,name,balance):
+        self.Name=name
+        self._balance=balance
+acc1=account("prakhar",10000)
+acc2=account("anuj",9000)
+print(acc1.Name)
+print(acc2._balance)
