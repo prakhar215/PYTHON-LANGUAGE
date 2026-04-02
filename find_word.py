3987e707cbf355a7a8f603fa47294e1584b7df8a
Prakhar <koshta215@gmail.com>
2026-04-02 13:35:27 +0530

    find word in sentence

---
+++ find_word.py
@@ -0,0 +1,10 @@
+data=True
+line =1
+word ="python"
+with open("sample.txt","r")as f:
+    while  data:
+        data=f.readline()
+        if (word in data):
+            print(f"{word} found at line {line}")
+            break
+        line+=1
