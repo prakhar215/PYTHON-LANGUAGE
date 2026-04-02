40a5f1876f0354cce146c863301cd9796ab404ce
Prakhar <koshta215@gmail.com>
2026-04-02 14:48:58 +0530

    List comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,3 +2,6 @@ squares=[]
 for i in range(6):
     squares.append(i*i)
 print(squares)
+#INSTEAD OF THIS SHORT METHOD IS LIST COMPREHENSION
+SQUARES=[ i*i for i in range(10) if i%2 == 0]
+print(SQUARES)
